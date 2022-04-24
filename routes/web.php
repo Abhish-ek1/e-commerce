17a25f2fe643f60902c23235c3424af99d84e475
abhishek <Abhisheksku75@gmail.com>
2022-04-24 12:49:00 +0530

    Updated

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('login');
 });
+
+Route::get('register', [UserController::class, 'register_open']);
+Route::get('save-user', [UserController::class, 'save_user']);
+Route::get('login-user', [UserController::class, 'login_user']);
